3e684c323a995f53d24497ff6a199dc6b88f08e3
Malave615 <malavetracy@gmail.com>
2024-07-31 13:19:10 -0400

    Added React structure
    Added React UI components: Card, Button, Link, Col, Row, Container.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,42 @@
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie, onMovieClick }) => (
-  // component code here
-  <Card className="h-100">
-    <Card.Img variant="top" src={movie.image} />
-    <Card.Body>
-      <Card.Title>{movie.title}</Card.Title>
-      <Card.Text>{movie.description}</Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <Button variant="link" onClick={() => onMovieClick(movie)}>
-        Open
-      </Button>
-    </Card.Footer>
-  </Card>
-);
+export const MovieCard = ({ movie, fav }) => {
+  const [isFav, setIsFav] = useState(fav);
+
+  useEffect(() => {
+    setIsFav(fav);
+  }, [fav]);
+
+  return (
+    <Card className="h-100">
+      <Card.Img
+        className="h-100"
+        variant="top"
+        src={movie.image}
+        alt={movie.Title}
+      />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>
+          {movie.description}
+          {isFav && <span className="fav-badge">Favorite</span>}
+        </Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+          <Button variant="link">Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
+};
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
+    Title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     genre: PropTypes.shape({
       name: PropTypes.string.isRequired,
@@ -34,5 +50,7 @@ MovieCard.propTypes = {
     featured: PropTypes.bool.isRequired,
     actors: PropTypes.arrayOf(PropTypes.string).isRequired,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired,
+  fav: PropTypes.bool.isRequired,
 };
+
+export default MovieCard;
