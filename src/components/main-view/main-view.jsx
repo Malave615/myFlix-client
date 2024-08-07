44e5078331440da2c5a8c095069ceb15c3e4b28c
Malave615 <malavetracy@gmail.com>
2024-08-07 12:36:00 -0400

    Redux
    Downloaded and installed Redux and Redux toolkit. Then incorporated redux into app in order to manage the state of the movies list and users.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,28 +1,24 @@
-import { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { LoginView } from '../login-view/login-view';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { MoviesList } from '../movies-list/movies-list';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
 import { SignupView } from '../signup-view/signup-view';
 import { NavigationBar } from '../navigation-bar/navigation-bar';
 import { ProfileView } from '../profile-view/profile-view';
-import { MovieCard } from '../movie-card/movie-card';
+
+import { setMovies } from '../../redux/reducers/movies';
 
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem('user'));
-  const storedToken = localStorage.getItem('token');
-  const [user, setUser] = useState(storedUser || null);
-  const [token, setToken] = useState(storedToken || null);
-  const [movies, setMovies] = useState([]);
-  const [favMovies, setFavMovies] = useState(user?.FavoriteMovies || []);
+  const movies = useSelector((state) => state.movies.list);
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!token) return;
-
-    fetch('https://tracys-movie-api-083e9c37dd14.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    fetch('https://tracys-movie-api-083e9c37dd14.herokuapp.com/movies')
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((movie) => ({
@@ -34,109 +30,49 @@ export const MainView = () => {
           director: movie.director,
           actors: movie.actors,
         }));
-        setMovies(moviesFromApi);
+
+        dispatch(setMovies(moviesFromApi));
       })
       .catch((error) => {
         console.error('Error fetching movies:', error);
       });
-  }, [token]);
+  }, [dispatch]);
 
-  useEffect(() => {
-    if (!user) setFavMovies([]);
-    else setFavMovies(user.FavoriteMovies || []);
-  }, [user]);
-
-  const renderContent = () => {
-    if (!user) {
-      return (
-        <Col md={5}>
-          <LoginView
-            onLoggedIn={(loggedInUser, loggedInUserToken) => {
-              setUser(loggedInUser);
-              setToken(loggedInUserToken);
-            }}
-          />
-          <p>or</p>
-          <SignupView />
-        </Col>
-      );
-    }
-
-    return (
-      <BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Row>
         <NavigationBar />
-        <Row className="justify-content-md-center">
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={8}>
           <Routes>
-            <Route
-              path="/signup"
-              element={
-                user ? (
-                  <Navigate to="/" />
-                ) : (
-                  <Col md={5}>
-                    <SignupView />
-                  </Col>
-                )
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                user ? (
-                  <Navigate to="/" />
-                ) : (
-                  <Col md={5}>
-                    <LoginView
-                      onLoggedIn={(loggedInUser) => setUser(loggedInUser)}
-                    />
-                  </Col>
-                )
-              }
-            />
-            {!user && <Navigate to="/login" replace />}
-            {user && movies.length === 0 && <Col>The list is empty!</Col>}
-            {user && movies.length > 0 && (
+            {!user && (
+              <>
+                <Route path="/signup" element={<SignupView />} />
+                <Route path="/login" element={<LoginView />} />
+                <Route path="/" element={<Navigate to="/login" />} />
+              </>
+            )}
+            {!user && (
               <>
                 <Route
-                  path="/movies/:movieId"
-                  element={
-                    <Col md={8}>
-                      <MovieView movies={movies} favMovies={favMovies} />
-                    </Col>
-                  }
-                />
-                <Route
                   path="/"
-                  element={movies.map((movie) => (
-                    <Col className="mb-4" key={movie.id} md={3}>
-                      <MovieCard
-                        movie={movie}
-                        fav={favMovies.includes(movie._id)}
-                      />
-                    </Col>
-                  ))}
-                />
-                <Route
-                  path="/profile"
                   element={
-                    !user ? (
-                      <Navigate to="/login" replace />
+                    movies.length === 0 ? (
+                      <Col>The list is empty!</Col>
                     ) : (
-                      <ProfileView
-                        movies={movies}
-                        user={user}
-                        setFavMovies={setFavMovies}
-                      />
+                      <MoviesList />
                     )
                   }
                 />
+                <Route path="/movies/:movieId" element={<MovieView />} />
+                <Route path="/profile" element={<ProfileView />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
               </>
             )}
           </Routes>
-        </Row>
-      </BrowserRouter>
-    );
-  };
-
-  return <Row className="justify-content-md-center">{renderContent()}</Row>;
+        </Col>
+      </Row>
+    </BrowserRouter>
+  );
 };
