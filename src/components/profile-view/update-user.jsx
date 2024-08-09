ce3d47590aee9951f472da9bab74e8a3a1383dca
Malave615 <malavetracy@gmail.com>
2024-08-09 13:05:15 -0400

    Updated the Form import
    Updated the Form import from React as instructed by Netlify in order to deploy app.

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form } from 'react-bootstrap/Form';
+import { Form } from 'react-bootstrap';
 
 const UpdateUser = ({ handleSubmit, handleUpdate, user }) => (
   <>
